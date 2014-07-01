ada8218138df8de251f189e4e13ed7862ae4cf25
Frances Burch <fburchdev@gmail.com>
2014-07-01 19:15:20 -0400

    Authors controller, view, routes

--- nbp_fb/config/routes.rb
+++ nbp_fb/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
-  resources :series_requests
+
+  resources :series_requests, :authors
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
