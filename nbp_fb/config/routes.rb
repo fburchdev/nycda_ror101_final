c35aa2e203bcd8defa575dfad968447e7ed5e029
Frances Burch <fburchdev@gmail.com>
2014-07-01 21:05:32 -0400

    Series controller, show view, and route

--- nbp_fb/config/routes.rb
+++ nbp_fb/config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 
 
-  resources :series_requests, :authors
+  resources :series_requests, :authors, :series
+  root 'authors#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
