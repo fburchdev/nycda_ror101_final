567908cc169473415ccb4e4bda40a2e5943164e8
Frances Burch <fburchdev@gmail.com>
2014-07-03 21:21:28 -0400

    Author name formatting in progress

--- nbp_fb/app/models/author.rb
+++ nbp_fb/app/models/author.rb
@@ -2,7 +2,18 @@ class Author < ActiveRecord::Base
 	has_many :series, dependent: :destroy
 
 	def self.get_name(author)
-	  author_name = 'Unknown'
+	  author_name = 'Unknown '
+	  if !(author.prefix.blank?)
+	  	author_name += author.prefix
+	  end
+	  if !(author.first_name.blank?)
+	  	author_name += author.first_name + ' '
+	  end
+
+	  
+
+
+
 	  return author_name
 	end
 end
