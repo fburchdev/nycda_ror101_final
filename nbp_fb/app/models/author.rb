637cc88e5c86cb748894a10730e7b27dfecacab2
Frances Burch <fburchdev@gmail.com>
2014-07-05 11:26:33 -0400

    Author name format method

--- nbp_fb/app/models/author.rb
+++ nbp_fb/app/models/author.rb
@@ -2,18 +2,22 @@ class Author < ActiveRecord::Base
 	has_many :series, dependent: :destroy
 
 	def self.get_name(author)
-	  author_name = 'Unknown '
+	  author_name = ''
 	  if !(author.prefix.blank?)
 	  	author_name += author.prefix
 	  end
 	  if !(author.first_name.blank?)
 	  	author_name += author.first_name + ' '
 	  end
-
-	  
-
-
-
-	  return author_name
+	  if !(author.middle_name.blank?)
+	  	author_name += author.middle_name + ' '
+	  end
+	  if !(author.last_name.blank?)
+	  	author_name += author.last_name + ' '
+	  end
+	  if !(author.suffix.blank?)
+	  	author_name += author.suffix
+	  end
+	  return author_name.chomp
 	end
 end
