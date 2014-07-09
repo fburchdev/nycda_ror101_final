ccd2c9a97ef55c7a680eca23bcfd1d17cbd6d99f
Frances Burch <fburchdev@gmail.com>
2014-07-09 00:22:24 -0400

    Author sort order

--- nbp_fb/app/controllers/authors_controller.rb
+++ nbp_fb/app/controllers/authors_controller.rb
@@ -1,6 +1,6 @@
 class AuthorsController < ApplicationController
   def index
-  	@authors = Author.all
+  	@authors = Author.order(:last_name, :first_name, :middle_name, :prefix, :suffix)
   end
 
   def show
